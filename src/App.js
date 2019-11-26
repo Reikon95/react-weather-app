75a69ab497c97d7d5617549a7856b745c8521f4e
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-26 09:49:26 +0000

    refresh not working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Weather from './Weather.js'
 import InputForm from './Form';
 
 const ApiKey = 'c3905f7a93aa6ca2b8cf8e68b9d6e718';
-// api.openweathermap.org/data/2.5/weather?q=London,uk
 class App extends React.Component {
   constructor() {
     super();
@@ -25,7 +24,7 @@ class App extends React.Component {
     let city = this.state.city;
 
     const apiCall = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${ApiKey}`
+        `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city},uk&appid=${ApiKey}`
     )
     const response = await apiCall.json();
     console.log(response);
@@ -38,13 +37,9 @@ class App extends React.Component {
       temp_min: parseInt(response.main.temp_min - 273)
     })
   }
-  clickHandler = (e) => {
-    console.log('click')
-  }
+
   submitFormHandler = e => {
-    e.preventDefault();
-    
-    console.log(this.ref.name.value); //will give us the name value
+    this.getWeather(e)
   }
 
   render() { 
@@ -54,18 +49,13 @@ class App extends React.Component {
      (
     <div className="App">
       Weather App
-      <InputForm/>
+      {/* <InputForm/> */}
       <Weather city={this.state.city} country={this.state.country} cel={this.state.cel} main={this.state.main} temp_min={this.state.temp_min} 
       temp_max={this.state.temp_max}/>
-      {/* <form>
-            <label>Enter your city here: </label><input className="form-val" name="city" type="text" placeholder="City"/>
-            <button onClick={this.clickHandler}>Submit</button>
-
-        </form> */}
       
         <form onSubmit={this.submitFormHandler}>
           <div>
-          <label>Enter your city here: </label><input type="text" name="name" ref="name" />
+          <label>Enter your city here: </label><input type="text" name="name"/>
           </div>
         </form>
 
