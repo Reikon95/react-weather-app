93cad01b3af13587a112d499cf4aa81aa44ce0ce
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-01 14:04:26 +0000

    conditional rendering of icons implemented

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'weather-icons/css/weather-icons.css';
+import './Weather.css'
 
 
 const Weather = (props) => {
@@ -8,7 +9,10 @@ const Weather = (props) => {
         <div className="container">
             <div className="place">
                 <h1>Welcome to {props.city}!</h1>
-                    <i className="wi wi-day-sunny display-1"/>
+                    {/* <h1 className="py-2">
+                    <i className="wi wi-day-cloudy display-1 weather-icon"/>
+                    </h1> */}
+                    {getIcon(props)}
                 {props.main}
                 <h1>{props.cel}°C</h1>
                 {minMaxTemp(props.temp_min, props.temp_max)}
@@ -27,6 +31,26 @@ function minMaxTemp(min, max) {
         <h3>Maximum: {max}</h3>
         </>
     )
+}
+
+function getIcon(props) {
+    return props.main === "Clouds" ? (
+        <h1 className="py-2">
+        <i className="wi wi-day-cloudy display-1 weather-icon"/>
+        </h1>
+    ) : props.main === "Rain" || props.main === "Drizzle" ? (
+        <h1 className="py-2">
+        <i className="wi wi-day-rain display-1 weather-icon"/>
+        </h1>
+    ) : props.main === "Sunny" || props.main === "Clear" ? (
+        <h1 className="py-2">
+        <i className="wi wi-day-sunny display-1 weather-icon"/>
+        </h1>       
+    ) : props.main === "Snow" ? (
+        <h1 className="py-2">
+        <i className="wi wi-day-snow display-1 weather-icon"/>
+        </h1>             
+    ) : <p>icon not available</p>
 }
 
 export default Weather
