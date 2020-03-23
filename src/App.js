5d63087f04a77ea761fbcaa130c2b3a34ee3a17c
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-23 17:35:40 +0000

    issues with quick rerender

--- src/App.js
+++ src/App.js
@@ -1,77 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Weather from './Weather.js'
 import AdvancedDetails from './AdvancedDetails.js';
-
-//app now deployed on heroku - access here https://weather-checker-cjwb.herokuapp.com/
-const ApiKey = 'c3905f7a93aa6ca2b8cf8e68b9d6e718';
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      city: 'London',
-      country: 'UK',
-      icon: undefined,
-      main: undefined,
-      cel: undefined,
-      temp_max: undefined,
-      temp_min: undefined,
-      description: "",
-      error: false
-    };
-    this.getWeather(this.state.city);
-  }
-  getWeather = async (city) => {
-    const apiCall = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
-    )
-    const response = await apiCall.json();
-    console.log(response);
-    this.setState({
-      city: `${response.name}`,
-      country: response.sys.country,
-      cel: parseInt((response.main.temp - 273)),
-      main: response.weather[0].main,
-      temp_max: parseInt(response.main.temp_max - 273),
-      temp_min: parseInt(response.main.temp_min - 273),
-      windSpeed: parseInt(response.wind.speed),
-      humidity: parseInt(response.main.humidity),
-      pressure: parseInt(response.main.pressure)
+export default function App() {
+    const ApiKey = 'c3905f7a93aa6ca2b8cf8e68b9d6e718';
+    let [forecast, setForecast] = useState({
+        city: 'London',
+        country: 'UK',
+        icon: undefined,
+        main: undefined,
+        cel: undefined,
+        temp_max: undefined,
+        temp_min: undefined,
+        description: "",
+        error: false
     })
-  }
-
-  submitFormHandler = e => {
-    e.preventDefault();
-    const cityInput = document.querySelector('.cityInput').value;
-    console.log('the input value is: ', cityInput);  
-    this.setState({
-      city: cityInput
-    }, () => this.getWeather(this.state.city) )
-    
-  }
-
-  render() { 
-    document.title="Cam Blackwood Weather App"
-    return this.state.city === undefined ?  
-      <h1>Hmm... appears that either you made a typo or we don't track your city yet. Try en  tering your nearest large city</h1>
-     :
-     (
-    <div className="App">
-      Cam Blackwood's Weather App
-      <Weather city={this.state.city} country={this.state.country} cel={this.state.cel} main={this.state.main} temp_min={this.state.temp_min} 
-      temp_max={this.state.temp_max}/>
-      
-        <form onSubmit={(e) => this.submitFormHandler(e)}>
-          <div>
-          <label>Enter your city here: </label><input className="cityInput" type="text"/>
-          </div>
-        </form>
-        <AdvancedDetails windSpeed={this.state.windSpeed} humidity={this.state.humidity} pressure={this.state.pressure}/>
+    async function getWeather(city) {
+        const apiCall = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
+        )
+        const response = await apiCall.json();
+        console.log(response);
+        setForecast({
+            city: `${response.name}`,
+            country: response.sys.country,
+            cel: parseInt((response.main.temp - 273)),
+            main: response.weather[0].main,
+            temp_max: parseInt(response.main.temp_max - 273),
+            temp_min: parseInt(response.main.temp_min - 273),
+            windSpeed: parseInt(response.wind.speed),
+            humidity: parseInt(response.main.humidity),
+            pressure: parseInt(response.main.pressure)
+        })
+    }
+    function submitFormHandler(e) {
+        const cityInput = document.querySelector('.cityInput').value;
+        console.log('the input value is: ', cityInput);
+        setForecast({
+            city: cityInput
+        })
+        getWeather(forecast.city)
         
-    </div>
-  );
-  }
+    }
+    return (
+        forecast.city === undefined ?
+            <h1>Hmm... appears that either you made a typo or we don't track your city yet. Try en  tering your nearest large city</h1>
+
+            :
+
+            <div className="App">
+                Cam Blackwood's Weather App
+      <Weather city={forecast.city} country={forecast.country} cel={forecast.cel} main={forecast.main} temp_min={forecast.temp_min}
+                    temp_max={forecast.temp_max} />
+
+                <form onSubmit={(e) => submitFormHandler(e)}>
+                    <div>
+                        <label>Enter your city here: </label><input className="cityInput" type="text" />
+                    </div>
+                </form>
+                <AdvancedDetails windSpeed={forecast.windSpeed} humidity={forecast.humidity} pressure={forecast.pressure} />
+
+            </div>
+    )
+
 }
-
-export default App;
-
--- src/functional.js
+++ src/functional.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Weather from './Weather.js'
 import AdvancedDetails from './AdvancedDetails.js';
-export default function FApp() {
+export default function App() {
     const ApiKey = 'c3905f7a93aa6ca2b8cf8e68b9d6e718';
     let [forecast, setForecast] = useState({
         city: 'London',
@@ -14,7 +14,7 @@ export default function FApp() {
         temp_min: undefined,
         description: "",
         error: false
-      })
+    })
     async function getWeather(city) {
         const apiCall = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
@@ -31,37 +31,37 @@ export default function FApp() {
             windSpeed: parseInt(response.wind.speed),
             humidity: parseInt(response.main.humidity),
             pressure: parseInt(response.main.pressure)
-          })
+        })
     }
     function submitFormHandler(e) {
         e.preventDefault();
         const cityInput = document.querySelector('.cityInput').value;
-        console.log('the input value is: ', cityInput);  
+        console.log('the input value is: ', cityInput);
         setForecast({
-          city: cityInput
-        }, () => getWeather(forecast.city) )
+            city: cityInput
+        }, () => getWeather(forecast.city))
     }
     return (
-        forecast.city === undefined ? 
-        <h1>Hmm... appears that either you made a typo or we don't track your city yet. Try en  tering your nearest large city</h1>
+        forecast.city === undefined ?
+            <h1>Hmm... appears that either you made a typo or we don't track your city yet. Try en  tering your nearest large city</h1>
 
-        :
-     
-    <div className="App">
-      Cam Blackwood's Weather App
-      <Weather city={forecast.city} country={forecast.country} cel={forecast.cel} main={forecast.main} temp_min={forecast.temp_min} 
-      temp_max={forecast.temp_max}/>
-      
-        <form onSubmit={(e) => this.submitFormHandler(e)}>
-          <div>
-          <label>Enter your city here: </label><input className="cityInput" type="text"/>
-          </div>
-        </form>
-        <AdvancedDetails windSpeed={forecast.windSpeed} humidity={forecast.humidity} pressure={forecast.pressure}/>
-        
-    </div>
-  )
-  
-     }
+            :
+
+            <div className="App">
+                Cam Blackwood's Weather App
+      <Weather city={forecast.city} country={forecast.country} cel={forecast.cel} main={forecast.main} temp_min={forecast.temp_min}
+                    temp_max={forecast.temp_max} />
+
+                <form onSubmit={(e) => submitFormHandler(e)}>
+                    <div>
+                        <label>Enter your city here: </label><input className="cityInput" type="text" />
+                    </div>
+                </form>
+                <AdvancedDetails windSpeed={forecast.windSpeed} humidity={forecast.humidity} pressure={forecast.pressure} />
+
+            </div>
+    )
+
+}
 
 
